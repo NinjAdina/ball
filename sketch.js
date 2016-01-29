feb724f3c8b0633c11f084dce5a7c4429761edf3
NinjAdina <AdinaShuttari@Adinas-MacBook-Pro.local>
2016-01-28 20:44:33 -0500

    Bounces Around

--- sketch.js
+++ sketch.js
@@ -12,7 +12,10 @@ var ball = {
   x: 20,
   y: 200,
   size: 15,
-  speed: 3,
+  speed: {
+    x: 3,
+    y: 3
+  },
 
   display: function () {
     // note that to access any of ball's properties, you must use dot notation, using the special keyword 'this'.
@@ -25,17 +28,24 @@ var ball = {
   },
 
   move: function () {
-    this.y += this.speed; // this line is the same as: this.x = this.x + this.speed;
+    this.x += this.speed.x; // this.x = this.x + this.speed.x
+    this.y += this.speed.y; // this line is the same as: this.x = this.x + this.speed;
   },
 
   checkForBounce: function () {
-    if (this.y > height - this.size / 2) this.bounce();
-    if (this.y < this.size / 2) this.bounce();
+    if (this.y > height - this.size / 2) this.bounceY();
+    if (this.y < this.size / 2) this.bounceY();
+    if (this.x > width - this.size / 2) this.bounceX();
+    if (this.x < this.size / 2) this.bounceX();
   },
 
-  bounce: function () {
+  bounceX: function () {
     // your code goes here; a hint: this will require making your own variable as well as defining this function
     // another hint: notice that ball is defined using object literal notation
-    this.speed *= -1;
+    this.speed.x *= -1; // this.speed = this.speed * -1;
+  },
+
+  bounceY: function() {
+    this.speed.y *= -1;
   }
 };
