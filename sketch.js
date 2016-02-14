ca21d35e56cdc7c87435dad578a9c826d88a9e14
NinjAdina <AdinaShuttari@Adinas-MacBook-Pro.local>
2016-02-14 16:42:55 -0500

    Added more notes

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ var draw = function() { // draw is called 60 times per second; animation goes he
 }
 
 var ball = {
-  x: 20,
-  y: 200,
-  size: 15,
-  speed: {
-    x: 3,
-    y: 3
+  x: 20, // this is the horizontal (x) point at which ball will start moving.
+  y: 200, // the vertical (y) point at which ball will start.
+  size: 15, // this will be the length and width of the ball.
+  speed: { // will reference this variable w/ 2 properties as speed.x or speed.y
+    x: 3, // need this to move ball horizontally.
+    y: 3 // need to add this to move ball vertically as it's moving horizontally = diagonally.
   },
 
   display: function () {
@@ -28,8 +28,8 @@ var ball = {
   },
 
   move: function () {
-    this.x += this.speed.x; // this.x = this.x + this.speed.x
-    this.y += this.speed.y; // this line is the same as: this.x = this.x + this.speed;
+    this.x += this.speed.x; // this line is the same as: this.x = this.x + this.speed.x;
+    this.y += this.speed.y; // this line is the same as: this.y = this.y + this.speed.y;
   },
 
   checkForBounce: function () {
@@ -42,10 +42,10 @@ var ball = {
   bounceX: function () {
     // your code goes here; a hint: this will require making your own variable as well as defining this function
     // another hint: notice that ball is defined using object literal notation
-    this.speed.x *= -1; // this.speed = this.speed * -1;
+    this.speed.x *= -1; // this line is the same as: this.speed.x = this.speed.x * -1;
   },
 
   bounceY: function() {
-    this.speed.y *= -1;
+    this.speed.y *= -1; // this line is the same as: this.speed.y = this.speed.y * -1;
   }
 };
